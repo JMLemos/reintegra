119100f28cd2944fdacec2dd0bc99840b774542a
ViniciusCHR <vinicius.unicap@gmail.com>
2021-11-17 13:17:16 -0300

    @ViniciusCHR / CriandoService e Adicionando metodos de login no controle Usuario

---
+++ src/main/java/com/reintegra/service/UsuarioService.java
@@ -0,0 +1,54 @@
+package com.reintegra.service;
+
+import java.nio.charset.Charset;
+import java.util.Optional;
+
+import org.apache.commons.codec.binary.Base64;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.reintegra.model.Usuario;
+import com.reintegra.model.UsuarioLogin;
+import com.reintegra.repository.UsuarioRepository;
+
+
+@Service
+public class UsuarioService {
+
+	@Autowired
+	private UsuarioRepository repository;
+
+	public Usuario CadastrarUsuario(Usuario usuario) {
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+		String senhaEncoder = encoder.encode(usuario.getSenha());
+		usuario.setSenha(senhaEncoder);
+
+		return repository.save(usuario);
+	}
+
+	public Optional<UsuarioLogin> Logar(Optional<UsuarioLogin> user) {
+
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		Optional<Usuario> usuario = repository.findByNomeCompleto(user.get().getNomeCompleto());
+
+		if (usuario.isPresent()) {
+			if (encoder.matches(user.get().getSenha(), usuario.get().getSenha())) {
+
+				String auth = user.get().getNomeCompleto() + ":" + user.get().getSenha();
+				byte[] encodedAuth = Base64.encodeBase64(auth.getBytes(Charset.forName("US-ASCII")));
+				String authHeader = "Basic " + new String(encodedAuth);
+
+				user.get().setToken(authHeader);				
+				user.get().setEmail(usuario.get().getEmail());
+				user.get().setSenha(usuario.get().getSenha());
+
+				return user;
+
+			}
+		}
+		return null;
+	}
+
+}
