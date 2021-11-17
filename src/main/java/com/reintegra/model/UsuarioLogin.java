424732a315c06823dc22fdb2f7fce1fdb169317d
unknown <cecilia.soares13@gmail.com>
2021-11-17 12:43:31 -0300

    @cecicsoares [CRIANDO USER CONTROLLER E PACKAGE SECURITY]

---
+++ src/main/java/com/reintegra/model/UsuarioLogin.java
@@ -0,0 +1,55 @@
+package com.reintegra.model;
+
+public class UsuarioLogin {
+
+	private String nomeCompleto;
+
+	private String email;
+
+	private String senha;
+
+	private String token;
+
+	private String tipo;
+
+	public String getNomeCompleto() {
+		return nomeCompleto;
+	}
+
+	public void setNomeCompleto(String nomeCompleto) {
+		this.nomeCompleto = nomeCompleto;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+}
